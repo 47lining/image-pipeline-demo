f01da68c0c3a3fb612a64a1fdac86beb5c11559d
Mark Chance <mark@peopledesigned.com>
2015-05-20 10:55:59 -0600

    move looker files, add license text

--- distributor/test/test_datematch.py
+++ distributor/test/test_datematch.py
@@ -1,3 +1,16 @@
+# Copyright 2015 47Lining LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import sys
 import datetime
 
@@ -17,7 +30,7 @@ if not StorySpecification.date_match(daten, {"min": "eq(30)"}):
 	print "FAIL: No match on min eq(30)"
 
 
-if not StorySpecification.date_match(daten, 
+if not StorySpecification.date_match(daten,
 	{"min": "eq(30)", "hour":"eq(12)"}):
 	print "FAIL: Not match on min eq(30) and hour eq(12)"
 if StorySpecification.date_match(daten,
--- orchestrator/orchestrate.py
+++ orchestrator/orchestrate.py
@@ -1,3 +1,16 @@
+# Copyright 2015 47Lining LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import uuid, time, boto, json, sys
 from boto.sqs.message import RawMessage
 from datetime import datetime, date, time, timedelta
@@ -42,7 +55,7 @@ def main(msgFn, queue, spec_file):
     td = end - start
     delta_second = td.total_seconds() / count
     time_now = start
-    td_incr = timedelta(seconds=delta_second) 
+    td_incr = timedelta(seconds=delta_second)
     for i in range(0, count):
         msg = time_now.strftime("%Y-%m-%d 00:00:00")
         msgFn(msg, queue)
