4d09d32072aeaf35c81eb898ac2f67e6b5268594
Mark Chance <mark@peopledesigned.com>
2015-05-21 17:27:29 -0600

    refactor for testability

--- distributor/application.py
+++ distributor/application.py
@@ -1,11 +1,11 @@
 # Copyright 2013. Amazon Web Services, Inc. All Rights Reserved.
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -66,6 +66,19 @@ def writeAnImage(record):
         record["longitude"], record["date_time"], tmpimagefolder+"/")
     QR.upload_file_to_s3(bucket_name, makeBucketKey(record), image_file_name, tmpimagefolder+"/")
 
+def handleMessage(message):
+    if "date" in message:
+        msg_date = message["date"]
+        if storySpec.generate_record(msg_date):
+            record = storySpec.create_record(msg_date)
+            # write the image
+            writeAnImage(record)
+        else:
+            print "Choosing not to write image for "+msg_date
+    elif "product_url" in message:
+        # write the image
+        writeAnImage(message)
+
 @application.route('/', methods=['POST'])
 def proc_message():
     response = None
@@ -73,29 +86,17 @@ def proc_message():
         # Expect application/json request
         response = Response("", status=415)
     else:
-        message = dict()
         try:
             # If the message has an SNS envelope, extract the inner message
             if request.json.has_key('TopicArn') and request.json.has_key('Message'):
                 message = json.loads(request.json['Message'])
             else:
                 message = request.json
-            if "date" in message:
-                msg_date = message["date"]
-                if storySpec.generate_record(msg_date):
-                    record = storySpec.create_record(msg_date)
-                    # write the image
-                    writeAnImage(record)
-                else:
-                    print "Choosing not to write image for "+msg_date
-            elif "product_url" in message:
-                # write the image
-                writeAnImage(message)
+            handleMessage(message)
             response = Response("", status=200)
         except Exception as ex:
             logging.exception('Error processing message: %s' % request.json)
             response = Response(ex.message, status=500)
-
     return response
 
 if __name__ == '__main__':
